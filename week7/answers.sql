5cab1f23d22be706b8e3782e70ed859ac1a4c91c
pedro-mc11 <123670067+pedro-mc11@users.noreply.github.com>
2024-12-13 19:13:07 -0800

    Created players table

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,3 +1,10 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA destruction;
 USE destruction;
+
+CREATE TABLE players (
+  player_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50)
+)
