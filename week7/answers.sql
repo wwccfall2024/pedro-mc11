4e0d434ed71b9c85c5afeb75d33552816b82f43f
pedro-mc11 <123670067+pedro-mc11@users.noreply.github.com>
2024-12-13 19:33:48 -0800

    Update answers.sql

--- week7/answers.sql
+++ week7/answers.sql
@@ -7,7 +7,7 @@ CREATE TABLE players (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50)
-)
+);
   
 CREATE TABLE characters (
   character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
